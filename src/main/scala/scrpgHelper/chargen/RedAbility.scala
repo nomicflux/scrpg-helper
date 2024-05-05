2b8d614fadd8bff42914b9c6ee76597e5eab6350
Michael A. <nomicflux@gmail.com>
2024-05-05 00:13:36 -0500

    first set of red abilities

--- src/main/scala/scrpgHelper/chargen/RedAbility.scala
+++ src/main/scala/scrpgHelper/chargen/RedAbility.scala
@@ -1,6 +1,7 @@
 package scrpgHelper.chargen
 
 import scrpgHelper.status.Status
+import scrpgHelper.rolls.Die
 
 case class RedAbility(
     abilityTemplate: AbilityTemplate,
@@ -12,10 +13,12 @@ object RedAbility:
   val redAbilityPhase: RedAbilityPhase = ()
 
   def allowedAbilityPool(qs: List[Quality], ps: List[Power]): AbilityPool =
-      baseRedAbilityPool.copy(id = baseRedAbilityPool.id,
-                              abilities = allRedAbilities
-                                  .filter(_.allowed(qs, ps))
-                                  .map(_.abilityTemplate))
+    baseRedAbilityPool.copy(
+      id = baseRedAbilityPool.id,
+      abilities = allRedAbilities
+        .filter(_.allowed(qs, ps))
+        .map(_.abilityTemplate)
+    )
 
   def apply(
       name: String,
@@ -39,46 +42,215 @@ object RedAbility:
           (qs, _ps) => qs.map(_.category).toSet.contains(qc)
     )
 
+  def withRestriction(
+      name: String,
+      abilityCategory: AbilityCategory,
+      actions: List[Action],
+      description: Description,
+      restriction: (List[Quality], List[Power]) => Boolean
+  ): RedAbility =
+    RedAbility(
+      AbilityTemplate(
+        name,
+        Status.Red,
+        abilityCategory,
+        _ => actions,
+        description
+      ),
+      restriction
+    )
+
   def allowedRedAbilities(
       qualities: List[Quality],
       powers: List[Power]
   ): List[AbilityTemplate] =
     allRedAbilities.filter(_.allowed(qualities, powers)).map(_.abilityTemplate)
 
-  val athleticAbility: RedAbility = RedAbility(
-    "Athletic",
+  val majorRegeneration: RedAbility = RedAbility.withRestriction(
+    "Major Regeneration",
     AbilityCategory.Action,
-    List(Action.Boost),
+    List(Action.Hinder, Action.Recover),
     List(
-      "Ability Test",
+      "Hinder yourself using",
+      PowerChoice(AbilityChoice.onePower(Power.vitality)),
+      ". Use your Min die. Recover health equal to you Max+Mid dice."
+    ),
+    (_, ps) => ps.contains(Power.vitality)
+  )
+
+  val paragonFeat: RedAbility = RedAbility(
+    "Paragon Feat",
+    AbilityCategory.Action,
+    List(Action.Overcome, Action.Boost),
+    List(
+      "Overcome using",
       PowerChoice(AbilityChoice.powerCategory(PowerCategory.Athletic)),
-      "."
+      "in a situation that requires you to be more than humanly capable, like an extreme feat of strength or speed. Use your Max+Min dice. Boost all reaby allies with your Mid die."
     ),
     PowerCategory.Athletic
   )
 
-  val energyAbility: RedAbility = RedAbility(
-    "Energy",
-    AbilityCategory.Action,
-    List(Action.Boost),
+  val pushYourLimits: RedAbility = RedAbility(
+    "Push Your Limits",
+    AbilityCategory.Inherent,
+    List(),
     List(
-      "Ability Test",
+      "You have no limit on amount of Reactions you can take. Each time you use a Reaction after the first one each turn, take 1 irreducible damage or take a minor twist."
+    ),
+    PowerCategory.Athletic
+  )
+
+  val reactiveStrike: RedAbility = RedAbility(
+    "Reactive Strike",
+    AbilityCategory.Reaction,
+    List(Action.Attack),
+    List(
+      "When you are Attacked and dealt damage, you may Attack the source of that damage by rolling your single",
+      PowerChoice(AbilityChoice.powerCategory(PowerCategory.Athletic)),
+      "die, plus the amount of damage you take"
+    ),
+    PowerCategory.Athletic
+  )
+
+  val athleticPowerAbilities: List[RedAbility] = List(
+    majorRegeneration,
+    paragonFeat,
+    pushYourLimits,
+    reactiveStrike
+  )
+
+  val chargedUpBlastEnergy: RedAbility = RedAbility(
+    "Charged Up Blast",
+    AbilityCategory.Action,
+    List(Action.Attack),
+    List(
+      "Attack using",
       PowerChoice(AbilityChoice.powerCategory(PowerCategory.Energy)),
-      "."
+      "and at least one bonus. Use your Max+Mid+Min dice. Destroy all of your bonuses, adding each of them to this Attack first, even if they are exclusive."
     ),
     PowerCategory.Energy
   )
 
-  val hallmarkAbility: RedAbility = RedAbility(
-    "Hallmark",
+   val eruption: RedAbility = RedAbility(
+    "Eruption",
     AbilityCategory.Action,
-    List(Action.Boost),
+    List(Action.Attack),
     List(
-      "Ability Test",
+      "Attack up to three targets, one of which must be you, using",
+      PowerChoice(AbilityChoice.powerCategory(PowerCategory.Energy)),
+      ". Assign your Min, Mid, and Max dice as you choose among those targets."
+    ),
+    PowerCategory.Energy
+  )
+
+   val improvedImmunity: RedAbility = RedAbility(
+    "Improved Immunity",
+    AbilityCategory.Inherent,
+    List(Action.Recover, Action.Boost),
+    List(
+      "If you would take damage from",
+      EnergyChoice(),
+      ", ignore that damage and Recover that amount instead. Use the value of the damage to Boost yourself."
+    ),
+    PowerCategory.Energy
+  )
+
+  val powerfulStrike: RedAbility = RedAbility(
+    "Powerful Strike",
+    AbilityCategory.Action,
+    List(Action.Attack),
+    List(
+      "Attack using",
+      PowerChoice(AbilityChoice.powerCategory(PowerCategory.Energy)),
+      ". Use your Max+Mid dice."
+    ),
+    PowerCategory.Energy
+  )
+
+  val purification: RedAbility = RedAbility(
+    "Purification",
+    AbilityCategory.Action,
+    List(),
+    List(
+      "Remove all bonuses and penalties from the scene. You cannot use this ability again this scene."
+    ),
+    PowerCategory.Energy
+  )
+
+  val summonedAllies: RedAbility = RedAbility(
+    "Summoned Allies",
+    AbilityCategory.Action,
+    List(),
+    List(
+      "Use",
+      PowerChoice(AbilityChoice.powerCategory(PowerCategory.Energy)),
+      s". to create a number of ${Die.d(6)} minions equal to your Mid die. Choose the one same basic action that they each perform. They all act at the start of your turn."
+    ),
+    PowerCategory.Energy
+  )
+
+  val energyPowerAbilities: List[RedAbility] = List(
+      chargedUpBlastEnergy,
+      eruption,
+      improvedImmunity,
+      powerfulStrike,
+      purification,
+      summonedAllies,
+  )
+
+  val chargedUpBlastHallmark: RedAbility = RedAbility.withRestriction(
+    "Charged Up Blast",
+    AbilityCategory.Action,
+    List(Action.Attack),
+    List(
+      "Attack using",
+      PowerChoice(AbilityChoice.onePower(Power.signatureWeapon)),
+      "and at least one bonus. Use your Max+Mid+Min dice. Destroy all of your bonuses, adding each of them to this Attack first, even if they are exclusive."
+    ),
+    (_qs, ps) => ps.contains(Power.signatureWeapon)
+  )
+
+  val quickExit: RedAbility = RedAbility.withRestriction(
+    "Quick Exit",
+    AbilityCategory.Action,
+    List(Action.Attack, Action.Hinder),
+    List(
+      "Attack using",
+      PowerChoice(AbilityChoice.onePower(Power.signatureVehicle)),
+      ". Use your Max die. Hinder each nearby opponent with your Mid die. After using this ability, you and up to 2 allies may end up anywhere in the scene, even outside of the action."
+    ),
+    (_qs, ps) => ps.contains(Power.signatureVehicle)
+  )
+
+  val sacrificialRam: RedAbility = RedAbility.withRestriction(
+    "Sacrificial Ram",
+    AbilityCategory.Action,
+    List(Action.Attack),
+    List(
+      "Attack up to three nearby targets using",
+      PowerChoice(AbilityChoice.onePower(Power.signatureVehicle)),
+      ". Use your Max+Mid dice against each of them. You cannot use your Signature Vehicle power for the rest of this scene and until it is recovered/repaired."
+    ),
+    (_qs, ps) => ps.contains(Power.signatureVehicle)
+  )
+
+  val ultimateWeaponry: RedAbility = RedAbility(
+    "Ultimate Weaponry",
+    AbilityCategory.Action,
+    List(Action.Boost, Action.Attack),
+    List(
+      "Boost yourself using",
       PowerChoice(AbilityChoice.powerCategory(PowerCategory.Hallmark)),
-      "."
+      ". Use your Max die. That bonus is persistent and exclusive. Attack using your Mid die plus that bonus."
     ),
     PowerCategory.Hallmark
+  )
+
+  val hallmarkPowerAbilities: List[RedAbility] = List(
+      chargedUpBlastHallmark,
+      quickExit,
+      sacrificialRam,
+      ultimateWeaponry,
   )
 
   val intellectualAbility: RedAbility = RedAbility(
@@ -201,24 +373,25 @@ object RedAbility:
     QualityCategory.Social
   )
 
-  val allRedAbilities: List[RedAbility] = List(
-    athleticAbility,
-    energyAbility,
-    hallmarkAbility,
-    intellectualAbility,
-    materialAbility,
-    selfControlAbility,
-    psychicAbility,
-    mobilityAbility,
-    technologicalAbility,
-    informationAbility,
-    mentalAbility,
-    physicalAbility,
-    socialAbility
-  )
+  val allRedAbilities: List[RedAbility] =
+    athleticPowerAbilities ++
+    energyPowerAbilities ++
+    hallmarkPowerAbilities ++
+      List(
+        intellectualAbility,
+        materialAbility,
+        selfControlAbility,
+        psychicAbility,
+        mobilityAbility,
+        technologicalAbility,
+        informationAbility,
+        mentalAbility,
+        physicalAbility,
+        socialAbility
+      )
 
   val redAbilityLookup: Map[AbilityId, RedAbility] =
-      allRedAbilities.map(a => a.abilityTemplate.id -> a).toMap
+    allRedAbilities.map(a => a.abilityTemplate.id -> a).toMap
 
   val baseRedAbilityPool: AbilityPool = AbilityPool(
     2,
