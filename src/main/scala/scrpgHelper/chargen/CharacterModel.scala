78796883075d10c26c56bfeefa25ed888983f67d
Michael A. <nomicflux@gmail.com>
2025-09-22 08:11:48 -0500

    slim down charactersignalmanager

--- src/main/scala/scrpgHelper/chargen/CharacterModel.scala
+++ src/main/scala/scrpgHelper/chargen/CharacterModel.scala
@@ -14,52 +14,6 @@ import scrpgHelper.chargen.characterModel.*
 
 final class CharacterModel extends SignalManager:
 
-  // Base data for initialization - computed values for signalManager
-  private val basePersonalityQualitiesVal: Map[StagingKey, List[(Quality, Die)]] =
-    Personality.personalities
-      .map(p => p -> List((p.baseQuality, Die.d(8))))
-      .toMap
-
-  private val powerSourceAbilitiesVal: List[(PowerSource, Map[AbilityKey, ChosenAbility])] =
-    PowerSource.powerSources
-      .map(ps =>
-        ps ->
-          (ps.abilityPools
-            .flatMap(ap =>
-              ap.abilities.map(a => a.key -> a.toChosenAbility(ap))
-            )
-            .toMap)
-      )
-
-  private val archetypeAbilitiesVal: List[(Archetype, Map[AbilityKey, ChosenAbility])] =
-    Archetype.archetypes
-      .map(at =>
-        at ->
-          (at.abilityPools
-            .flatMap(ap =>
-              ap.abilities.map(a => a.key -> a.toChosenAbility(ap))
-            )
-            .toMap)
-      )
-
-  private val personalityAbilitiesVal: List[(Personality, Map[AbilityKey, ChosenAbility])] =
-    Personality.personalities
-      .map(pt =>
-        pt -> (pt.outAbilityPool.abilities
-          .map(a => a.key -> a.toChosenAbility(pt.outAbilityPool))
-          .toMap)
-      )
-
-  private val redAbilitiesVal: List[(RedAbility.RedAbilityPhase, Map[AbilityKey, ChosenAbility])] =
-    List(
-      RedAbility.redAbilityPhase -> RedAbility.baseRedAbilityPool.abilities
-        .map(a => a.key -> a.toChosenAbility(RedAbility.baseRedAbilityPool))
-        .toMap
-    )
-
-  private val baseAbilitiesParam: Map[StagingKey, Map[AbilityKey, ChosenAbility]] =
-    (powerSourceAbilitiesVal ++ archetypeAbilitiesVal ++ personalityAbilitiesVal ++ redAbilitiesVal).toMap
-
   // Reactive state variables
   val backgroundVar: Var[Option[Background]] = Var(None)
   val powerSourceVar: Var[Option[PowerSource]] = Var(None)
@@ -67,45 +21,31 @@ final class CharacterModel extends SignalManager:
   val personalityVar: Var[Option[Personality]] = Var(None)
   val healthVar: Var[Option[Int]] = Var(None)
 
-  // Reactive staging vars
-  val qualityStagingVar: Var[Map[StagingKey, List[(Quality, Die)]]] = Var(basePersonalityQualitiesVal)
+  // Reactive staging vars - initialized with base values that will be computed by signalManager
+  val qualityStagingVar: Var[Map[StagingKey, List[(Quality, Die)]]] = Var(
+    Personality.personalities.map(p => p -> List((p.baseQuality, Die.d(8)))).toMap
+  )
   val powerStagingVar: Var[Map[StagingKey, List[(Power, Die)]]] = Var(Map())
   val abilityStagingVar: Var[Map[StagingKey, List[Ability[_]]]] = Var(Map())
-  val abilityChoiceVar: Var[Map[StagingKey, Map[AbilityKey, ChosenAbility]]] = Var(baseAbilitiesParam)
+  val abilityChoiceVar: Var[Map[StagingKey, Map[AbilityKey, ChosenAbility]]] = Var({
+    val powerSourceAbilities = PowerSource.powerSources.map(ps =>
+      ps -> ps.abilityPools.flatMap(ap => ap.abilities.map(a => a.key -> a.toChosenAbility(ap))).toMap
+    )
+    val archetypeAbilities = Archetype.archetypes.map(at =>
+      at -> at.abilityPools.flatMap(ap => ap.abilities.map(a => a.key -> a.toChosenAbility(ap))).toMap
+    )
+    val personalityAbilities = Personality.personalities.map(pt =>
+      pt -> pt.outAbilityPool.abilities.map(a => a.key -> a.toChosenAbility(pt.outAbilityPool)).toMap
+    )
+    val redAbilities = List(
+      RedAbility.redAbilityPhase -> RedAbility.baseRedAbilityPool.abilities
+        .map(a => a.key -> a.toChosenAbility(RedAbility.baseRedAbilityPool)).toMap
+    )
+    (powerSourceAbilities ++ archetypeAbilities ++ personalityAbilities ++ redAbilities).toMap
+  })
   val dieChangesVar: Var[Map[StagingKey, Map[Quality | Power, DieChange]]] = Var(Map())
 
-  // Create observer functions that don't depend on signalManager
-  private val backgroundObserver = backgroundVar.updater { (_, b: Background) => Some(b) }
-  private val powerSourceObserver = powerSourceVar.updater { (_, ps: PowerSource) => Some(ps) }
-  private val archetypeObserver = archetypeVar.updater { (_, at: Archetype) => Some(at) }
-  private val healthObserver = healthVar.updater { (m, n: Int) => if (m.isDefined) then m else Some(n) }
-
-  // Create personality observer that will be defined after signalManager
-  private def personalityObserver = personalityVar.updater { (_, p: Personality) =>
-    // Toggle ability will be handled by the signalManager after it's created
-    Some(p)
-  }
-
-  // Create a combined signal that triggers when any relevant data changes
-  private val validationTrigger: Signal[Unit] = backgroundVar.signal
-    .combineWith(
-      powerSourceVar.signal,
-      archetypeVar.signal,
-      personalityVar.signal,
-      healthVar.signal
-    )
-    .combineWith(
-      qualityStagingVar.signal,
-      powerStagingVar.signal,
-      abilityStagingVar.signal
-    )
-    .combineWith(
-      abilityChoiceVar.signal,
-      dieChangesVar.signal
-    )
-    .map { _ => () }
-
-  // Create signal manager to handle reactive signals
+  // Create signal manager to handle reactive signals with simplified constructor
   private lazy val signalManager = CharacterSignalManager(
     backgroundVar,
     powerSourceVar,
@@ -117,135 +57,7 @@ final class CharacterModel extends SignalManager:
     abilityStagingVar,
     abilityChoiceVar,
     dieChangesVar,
-    () => data,
-    basePersonalityQualitiesVal,
-    powerSourceAbilitiesVal,
-    archetypeAbilitiesVal,
-    personalityAbilitiesVal,
-    redAbilitiesVal,
-    baseAbilitiesParam,
-    backgroundObserver,
-    powerSourceObserver,
-    archetypeObserver,
-    personalityObserver,
-    healthObserver,
-    (stagingKey: StagingKey) => qualityStagingVar.updater { case (m, (q, d)) =>
-      val newList = m.getOrElse(stagingKey, List()) :+ (q, d)
-      if (newList == newList.distinct) {
-        m + (stagingKey -> newList)
-      } else {
-        m
-      }
-    },
-    (stagingKey: StagingKey) => qualityStagingVar.updater { case (m, (q, d)) =>
-      val newList = m.getOrElse(stagingKey, List()).filter(_._1 != q)
-      m + (stagingKey -> newList)
-    },
-    (stagingKey: StagingKey) => powerStagingVar.updater { case (m, (p, d)) =>
-      val newList = m.getOrElse(stagingKey, List()) :+ (p, d)
-      m + (stagingKey -> newList)
-    },
-    (stagingKey: StagingKey) => powerStagingVar.updater { case (m, (p, d)) =>
-      val newList = m.getOrElse(stagingKey, List()).filter(_._1 != p)
-      m + (stagingKey -> newList)
-    },
-    (stagingKey: StagingKey) => abilityStagingVar.updater { (m, a) =>
-      val newList = m.getOrElse(stagingKey, List()) :+ a
-      m + (stagingKey -> newList)
-    },
-    (stagingKey: StagingKey) => abilityStagingVar.updater { (m, a) =>
-      val newList = m.getOrElse(stagingKey, List()).filter(_.key != a.key)
-      m + (stagingKey -> newList)
-    },
-    (stagingKey: StagingKey) => abilityStagingVar.updater { (m, a) =>
-      val currList = m.getOrElse(stagingKey, List())
-      val currListKeys: Set[AbilityKey] = currList.map(_.key).toSet
-      val newList =
-        if (currListKeys.contains(a.key) && a.status != Status.Out) then
-          currList.filter(_.key != a.key)
-        else (currList :+ a)
-      if a.inPool.runValidation(newList.collect { case ca: ChosenAbility =>
-          ca
-        })
-      then m + (stagingKey -> newList)
-      else m
-    },
-    (stagingKey: StagingKey, ability: AbilityTemplate) => abilityChoiceVar.updater { (acs, choice) =>
-      val choices: Map[AbilityKey, ChosenAbility] =
-        acs.getOrElse(stagingKey, Map())
-      val mCurrChoice: Option[ChosenAbility] =
-        choices.get(ability.key).headOption
-      mCurrChoice.fold(acs) { currChoice =>
-        acs + (stagingKey -> (choices + (ability.key -> currChoice.applyChoice(
-          choice
-        ))))
-      }
-    },
-    (stagingKey: StagingKey, ability: AbilityTemplate) => abilityChoiceVar.updater { (acs, choice) =>
-      val choices: Map[AbilityKey, ChosenAbility] =
-        acs.getOrElse(stagingKey, Map())
-      val mCurrChoice: Option[ChosenAbility] = choices.get(ability.key)
-      mCurrChoice.fold(acs) { currChoice =>
-        acs + (stagingKey -> (choices + (ability.key -> currChoice.removeChoice(
-          choice
-        ))))
-      }
-    },
-    (key: StagingKey, direction: DieChange) => dieChangesVar.updater { (m, pq) =>
-      val currForKey: Map[Quality | Power, DieChange] = m.getOrElse(key, Map())
-      val changed: Option[DieChange] = currForKey.get(pq)
-      val updated = CharacterComputation.DieChange.combine(changed, Some(direction))
-      updated.fold(m + (key -> (currForKey - pq)))(dc =>
-        m + (key -> (currForKey + (pq -> dc)))
-      )
-    },
-    (key: StagingKey) => dieChangesVar.updater { (m, pq) =>
-      val currForKey: Map[Quality | Power, DieChange] = m.getOrElse(key, Map())
-      val changed: Option[DieChange] = currForKey.get(pq)
-      val updated = CharacterComputation.DieChange.combine(changed, Some(CharacterComputation.DieChange.Upgrade))
-      updated.fold(m + (key -> (currForKey - pq)))(dc =>
-        m + (key -> (currForKey + (pq -> dc)))
-      )
-    },
-    (key: StagingKey) => dieChangesVar.updater { (m, pq) =>
-      val currForKey: Map[Quality | Power, DieChange] = m.getOrElse(key, Map())
-      val changed: Option[DieChange] = currForKey.get(pq)
-      val updated = CharacterComputation.DieChange.combine(changed, Some(CharacterComputation.DieChange.Downgrade))
-      updated.fold(m + (key -> (currForKey - pq)))(dc =>
-        m + (key -> (currForKey + (pq -> dc)))
-      )
-    },
-    allDieChangesFunc,
-    validationTrigger.map { _ =>
-      val v = CharacterValidator(data)
-      v.validBackground(data)
-    },
-    validationTrigger.map { _ =>
-      val v = CharacterValidator(data)
-      v.validPowerSource(data)
-    },
-    validationTrigger.map { _ =>
-      val v = CharacterValidator(data)
-      v.validArchetype(data)
-    },
-    validationTrigger.map { _ =>
-      val v = CharacterValidator(data)
-      v.validPersonality(data)
-    },
-    validationTrigger.map { _ =>
-      val v = CharacterValidator(data)
-      v.validRedAbilities(data)
-    },
-    healthVar.signal.map { _ =>
-      val v = CharacterValidator(data)
-      v.validHealth(data)
-    },
-    validationTrigger
-      .combineWith(qualityStagingVar.signal, powerStagingVar.signal, abilityStagingVar.signal)
-      .map { _ =>
-        val e = CharacterExporter(data)
-        e.exportData(data)
-      }
+    () => data
   )
 
   // Change observers for core character selections (passthrough)
@@ -276,7 +88,7 @@ final class CharacterModel extends SignalManager:
       archetype,
       personality,
       dieChanges,
-      allDieChangesFunc
+      signalManager.allDieChanges
     )
 
     def allPowers: List[(Power, Die)] = CharacterComputation.computeAllPowers(
@@ -286,7 +98,7 @@ final class CharacterModel extends SignalManager:
       archetype,
       personality,
       dieChanges,
-      allDieChangesFunc
+      signalManager.allDieChanges
     )
 
     def allStagedAbilities: List[ChosenAbility] = CharacterComputation.computeAllStagedAbilities(
@@ -332,19 +144,7 @@ final class CharacterModel extends SignalManager:
     def abilityStaging: Map[StagingKey, List[Ability[_]]] = abilityStagingVar.now()
     def abilityChoice: Map[StagingKey, Map[AbilityKey, ChosenAbility]] = abilityChoiceVar.now()
     def dieChanges: Map[StagingKey, Map[Quality | Power, DieChange]] = dieChangesVar.now()
-    def baseAbilities: Map[StagingKey, Map[AbilityKey, ChosenAbility]] = baseAbilitiesParam
-
-  // allDieChanges function for signalManager constructor
-  private val allDieChangesFunc = (
-      dcs: Map[StagingKey, Map[Quality | Power, DieChange]],
-      mps: Option[PowerSource],
-      mat: Option[Archetype],
-      mpt: Option[Personality]
-  ) => (mps.fold(List())(ps => dcs.get(ps).fold(List())(dcps => dcps.toList)) ++
-      mat.fold(List())(at => dcs.get(at).fold(List())(dcat => dcat.toList)) ++
-      mpt.fold(List())(pt =>
-        dcs.get(pt).fold(List())(dcpt => dcpt.toList)
-      )).toMap
+    def baseAbilities: Map[StagingKey, Map[AbilityKey, ChosenAbility]] = signalManager.baseAbilitiesVal
 
 
   // SignalManager staging operations implementation (passthrough)
@@ -422,10 +222,6 @@ final class CharacterModel extends SignalManager:
 
 
   val calcHealth: Observer[Int] = signalManager.calcHealth
-
-  // Create lazy validator and exporter that work with current data
-  private lazy val validator = CharacterValidator(data)
-  private lazy val exporter = CharacterExporter(data)
 
   // SignalManager interface implementation (passthrough)
   val basePersonalityQualities: Map[StagingKey, List[(Quality, Die)]] = signalManager.basePersonalityQualities
--- src/main/scala/scrpgHelper/chargen/characterModel/CharacterSignalManager.scala
+++ src/main/scala/scrpgHelper/chargen/characterModel/CharacterSignalManager.scala
@@ -2,61 +2,138 @@ package scrpgHelper.chargen.characterModel
 
 import com.raquo.laminar.api.L.{*, given}
 import scrpgHelper.rolls.Die
+import scrpgHelper.status.Status
 import scrpgHelper.chargen.*
 
 /** Pure reactive signal management for character data.
   *
   * Handles all reactive signals and computed values derived from character state.
   * Provides the reactive interface while delegating business logic to pure functions.
+  *
+  * Simplified constructor - calculates all derived data internally.
   */
 class CharacterSignalManager(
+    // Core reactive state
     backgroundVar: Var[Option[Background]],
     powerSourceVar: Var[Option[PowerSource]],
     archetypeVar: Var[Option[Archetype]],
     personalityVar: Var[Option[Personality]],
     healthVar: Var[Option[Int]],
+
+    // Staging state
     val qualityStagingVar: Var[Map[CharacterComputation.StagingKey, List[(Quality, Die)]]],
     val powerStagingVar: Var[Map[CharacterComputation.StagingKey, List[(Power, Die)]]],
     val abilityStagingVar: Var[Map[CharacterComputation.StagingKey, List[Ability[_]]]],
     val abilityChoiceVar: Var[Map[CharacterComputation.StagingKey, Map[AbilityKey, ChosenAbility]]],
     val dieChangesVar: Var[Map[CharacterComputation.StagingKey, Map[Quality | Power, CharacterComputation.DieChange]]],
-    dataProvider: () => CharacterData,
-    // Required data for SignalManager
-    basePersonalityQualitiesVal: Map[CharacterComputation.StagingKey, List[(Quality, Die)]],
-    powerSourceAbilitiesVal: List[(PowerSource, Map[AbilityKey, ChosenAbility])],
-    archetypeAbilitiesVal: List[(Archetype, Map[AbilityKey, ChosenAbility])],
-    personalityAbilitiesVal: List[(Personality, Map[AbilityKey, ChosenAbility])],
-    redAbilitiesVal: List[(RedAbility.RedAbilityPhase, Map[AbilityKey, ChosenAbility])],
-    baseAbilitiesParam: Map[CharacterComputation.StagingKey, Map[AbilityKey, ChosenAbility]],
-    // Observers from CharacterModel
-    changeBackgroundVal: Observer[Background],
-    changePowerSourceVal: Observer[PowerSource],
-    changeArchetypeVal: Observer[Archetype],
-    changePersonalityVal: Observer[Personality],
-    calcHealthVal: Observer[Int],
-    // Observer generators from CharacterModel
-    addQualityFunc: CharacterComputation.StagingKey => Observer[(Quality, Die)],
-    removeQualityFunc: CharacterComputation.StagingKey => Observer[(Quality, Die)],
-    addPowerFunc: CharacterComputation.StagingKey => Observer[(Power, Die)],
-    removePowerFunc: CharacterComputation.StagingKey => Observer[(Power, Die)],
-    addAbilityFunc: CharacterComputation.StagingKey => Observer[Ability[_]],
-    removeAbilityFunc: CharacterComputation.StagingKey => Observer[Ability[_]],
-    toggleAbilityFunc: CharacterComputation.StagingKey => Observer[ChosenAbility],
-    addAbilityChoiceFunc: (CharacterComputation.StagingKey, AbilityTemplate) => Observer[AbilityChoice],
-    removeAbilityChoiceFunc: (CharacterComputation.StagingKey, AbilityTemplate) => Observer[AbilityChoice],
-    changeDieChangesFunc: (CharacterComputation.StagingKey, CharacterComputation.DieChange) => Observer[Quality | Power],
-    upgradeFunc: CharacterComputation.StagingKey => Observer[Quality | Power],
-    downgradeFunc: CharacterComputation.StagingKey => Observer[Quality | Power],
-    allDieChangesFunc: (Map[CharacterComputation.StagingKey, Map[Quality | Power, CharacterComputation.DieChange]], Option[PowerSource], Option[Archetype], Option[Personality]) => Map[Quality | Power, CharacterComputation.DieChange],
-    // Validation and export signals
-    validBackgroundVal: Signal[Boolean],
-    validPowerSourceVal: Signal[Boolean],
-    validArchetypeVal: Signal[Boolean],
-    validPersonalityVal: Signal[Boolean],
-    validRedAbilitiesVal: Signal[Boolean],
-    validHealthVal: Signal[Boolean],
-    forExportVal: Signal[CharacterModelExport]
+
+    // Data access
+    dataProvider: () => CharacterData
 ) extends SignalManager:
+
+  // Internal static data calculations
+  private val basePersonalityQualitiesVal: Map[StagingKey, List[(Quality, Die)]] =
+    Personality.personalities
+      .map(p => p -> List((p.baseQuality, Die.d(8))))
+      .toMap
+
+  private val powerSourceAbilitiesVal: List[(PowerSource, Map[AbilityKey, ChosenAbility])] =
+    PowerSource.powerSources
+      .map(ps =>
+        ps ->
+          (ps.abilityPools
+            .flatMap(ap =>
+              ap.abilities.map(a => a.key -> a.toChosenAbility(ap))
+            )
+            .toMap)
+      )
+
+  private val archetypeAbilitiesVal: List[(Archetype, Map[AbilityKey, ChosenAbility])] =
+    Archetype.archetypes
+      .map(at =>
+        at ->
+          (at.abilityPools
+            .flatMap(ap =>
+              ap.abilities.map(a => a.key -> a.toChosenAbility(ap))
+            )
+            .toMap)
+      )
+
+  private val personalityAbilitiesVal: List[(Personality, Map[AbilityKey, ChosenAbility])] =
+    Personality.personalities
+      .map(pt =>
+        pt -> (pt.outAbilityPool.abilities
+          .map(a => a.key -> a.toChosenAbility(pt.outAbilityPool))
+          .toMap)
+      )
+
+  private val redAbilitiesVal: List[(RedAbility.RedAbilityPhase, Map[AbilityKey, ChosenAbility])] =
+    List(
+      RedAbility.redAbilityPhase -> RedAbility.baseRedAbilityPool.abilities
+        .map(a => a.key -> a.toChosenAbility(RedAbility.baseRedAbilityPool))
+        .toMap
+    )
+
+  private val baseAbilitiesParam: Map[StagingKey, Map[AbilityKey, ChosenAbility]] =
+    (powerSourceAbilitiesVal ++ archetypeAbilitiesVal ++ personalityAbilitiesVal ++ redAbilitiesVal).toMap
+
+  // Internal observer creation
+  private val changeBackgroundVal = backgroundVar.updater { (_, b: Background) => Some(b) }
+  private val changePowerSourceVal = powerSourceVar.updater { (_, ps: PowerSource) => Some(ps) }
+  private val changeArchetypeVal = archetypeVar.updater { (_, at: Archetype) => Some(at) }
+  private val changePersonalityVal = personalityVar.updater { (_, p: Personality) => Some(p) }
+  private val calcHealthVal = healthVar.updater { (m, n: Int) => if (m.isDefined) then m else Some(n) }
+
+  // Internal validation trigger
+  private val validationTrigger: Signal[Unit] = backgroundVar.signal
+    .combineWith(
+      powerSourceVar.signal,
+      archetypeVar.signal,
+      personalityVar.signal,
+      healthVar.signal
+    )
+    .combineWith(
+      qualityStagingVar.signal,
+      powerStagingVar.signal,
+      abilityStagingVar.signal
+    )
+    .combineWith(
+      abilityChoiceVar.signal,
+      dieChangesVar.signal
+    )
+    .map { _ => () }
+
+  // Internal validation and export signals
+  private val validBackgroundVal: Signal[Boolean] = validationTrigger.map { _ =>
+    val v = CharacterValidator(dataProvider())
+    v.validBackground(dataProvider())
+  }
+  private val validPowerSourceVal: Signal[Boolean] = validationTrigger.map { _ =>
+    val v = CharacterValidator(dataProvider())
+    v.validPowerSource(dataProvider())
+  }
+  private val validArchetypeVal: Signal[Boolean] = validationTrigger.map { _ =>
+    val v = CharacterValidator(dataProvider())
+    v.validArchetype(dataProvider())
+  }
+  private val validPersonalityVal: Signal[Boolean] = validationTrigger.map { _ =>
+    val v = CharacterValidator(dataProvider())
+    v.validPersonality(dataProvider())
+  }
+  private val validRedAbilitiesVal: Signal[Boolean] = validationTrigger.map { _ =>
+    val v = CharacterValidator(dataProvider())
+    v.validRedAbilities(dataProvider())
+  }
+  private val validHealthVal: Signal[Boolean] = healthVar.signal.map { _ =>
+    val v = CharacterValidator(dataProvider())
+    v.validHealth(dataProvider())
+  }
+  private val forExportVal: Signal[CharacterModelExport] = validationTrigger
+    .combineWith(qualityStagingVar.signal, powerStagingVar.signal, abilityStagingVar.signal)
+    .map { _ =>
+      val e = CharacterExporter(dataProvider())
+      e.exportData(dataProvider())
+    }
 
   // SignalManager interface implementation - core character selections
   val background: Var[Option[Background]] = backgroundVar
@@ -71,27 +148,116 @@ class CharacterSignalManager(
   val changeArchetype: Observer[Archetype] = changeArchetypeVal
   val changePersonality: Observer[Personality] = changePersonalityVal
 
-  // Interface implementation via functions - delegate to passed-in functions
-  def addQuality(stagingKey: StagingKey): Observer[(Quality, Die)] = addQualityFunc(stagingKey)
-  def removeQuality(stagingKey: StagingKey): Observer[(Quality, Die)] = removeQualityFunc(stagingKey)
-  def addPower(stagingKey: StagingKey): Observer[(Power, Die)] = addPowerFunc(stagingKey)
-  def removePower(stagingKey: StagingKey): Observer[(Power, Die)] = removePowerFunc(stagingKey)
-  def addAbility(stagingKey: StagingKey): Observer[Ability[_]] = addAbilityFunc(stagingKey)
-  def removeAbility(stagingKey: StagingKey): Observer[Ability[_]] = removeAbilityFunc(stagingKey)
-  def toggleAbility(stagingKey: StagingKey): Observer[ChosenAbility] = toggleAbilityFunc(stagingKey)
-  def addAbilityChoice(stagingKey: StagingKey, ability: AbilityTemplate): Observer[AbilityChoice] = addAbilityChoiceFunc(stagingKey, ability)
-  def removeAbilityChoice(stagingKey: StagingKey, ability: AbilityTemplate): Observer[AbilityChoice] = removeAbilityChoiceFunc(stagingKey, ability)
-  def changeDieChanges(key: StagingKey, direction: DieChange): Observer[Quality | Power] = changeDieChangesFunc(key, direction)
-  def upgrade(key: StagingKey): Observer[Quality | Power] = upgradeFunc(key)
-  def downgrade(key: StagingKey): Observer[Quality | Power] = downgradeFunc(key)
+  // Internal observer function implementations
+  def addQuality(stagingKey: StagingKey): Observer[(Quality, Die)] =
+    qualityStagingVar.updater { case (m, (q, d)) =>
+      val newList = m.getOrElse(stagingKey, List()) :+ (q, d)
+      if (newList == newList.distinct) {
+        m + (stagingKey -> newList)
+      } else {
+        m
+      }
+    }
+
+  def removeQuality(stagingKey: StagingKey): Observer[(Quality, Die)] =
+    qualityStagingVar.updater { case (m, (q, d)) =>
+      val newList = m.getOrElse(stagingKey, List()).filter(_._1 != q)
+      m + (stagingKey -> newList)
+    }
+
+  def addPower(stagingKey: StagingKey): Observer[(Power, Die)] =
+    powerStagingVar.updater { case (m, (p, d)) =>
+      val newList = m.getOrElse(stagingKey, List()) :+ (p, d)
+      m + (stagingKey -> newList)
+    }
+
+  def removePower(stagingKey: StagingKey): Observer[(Power, Die)] =
+    powerStagingVar.updater { case (m, (p, d)) =>
+      val newList = m.getOrElse(stagingKey, List()).filter(_._1 != p)
+      m + (stagingKey -> newList)
+    }
+
+  def addAbility(stagingKey: StagingKey): Observer[Ability[_]] =
+    abilityStagingVar.updater { (m, a) =>
+      val newList = m.getOrElse(stagingKey, List()) :+ a
+      m + (stagingKey -> newList)
+    }
+
+  def removeAbility(stagingKey: StagingKey): Observer[Ability[_]] =
+    abilityStagingVar.updater { (m, a) =>
+      val newList = m.getOrElse(stagingKey, List()).filter(_.key != a.key)
+      m + (stagingKey -> newList)
+    }
+
+  def toggleAbility(stagingKey: StagingKey): Observer[ChosenAbility] =
+    abilityStagingVar.updater { (m, a) =>
+      val currList = m.getOrElse(stagingKey, List())
+      val currListKeys: Set[AbilityKey] = currList.map(_.key).toSet
+      val newList =
+        if (currListKeys.contains(a.key) && a.status != Status.Out) then
+          currList.filter(_.key != a.key)
+        else (currList :+ a)
+      if a.inPool.runValidation(newList.collect { case ca: ChosenAbility =>
+          ca
+        })
+      then m + (stagingKey -> newList)
+      else m
+    }
+
+  def addAbilityChoice(stagingKey: StagingKey, ability: AbilityTemplate): Observer[AbilityChoice] =
+    abilityChoiceVar.updater { (acs, choice) =>
+      val choices: Map[AbilityKey, ChosenAbility] =
+        acs.getOrElse(stagingKey, Map())
+      val mCurrChoice: Option[ChosenAbility] =
+        choices.get(ability.key).headOption
+      mCurrChoice.fold(acs) { currChoice =>
+        acs + (stagingKey -> (choices + (ability.key -> currChoice.applyChoice(
+          choice
+        ))))
+      }
+    }
+
+  def removeAbilityChoice(stagingKey: StagingKey, ability: AbilityTemplate): Observer[AbilityChoice] =
+    abilityChoiceVar.updater { (acs, choice) =>
+      val choices: Map[AbilityKey, ChosenAbility] =
+        acs.getOrElse(stagingKey, Map())
+      val mCurrChoice: Option[ChosenAbility] = choices.get(ability.key)
+      mCurrChoice.fold(acs) { currChoice =>
+        acs + (stagingKey -> (choices + (ability.key -> currChoice.removeChoice(
+          choice
+        ))))
+      }
+    }
+
+  def changeDieChanges(key: StagingKey, direction: DieChange): Observer[Quality | Power] =
+    dieChangesVar.updater { (m, pq) =>
+      val currForKey: Map[Quality | Power, DieChange] = m.getOrElse(key, Map())
+      val changed: Option[DieChange] = currForKey.get(pq)
+      val updated = CharacterComputation.DieChange.combine(changed, Some(direction))
+      updated.fold(m + (key -> (currForKey - pq)))(dc =>
+        m + (key -> (currForKey + (pq -> dc)))
+      )
+    }
+
+  def upgrade(key: StagingKey): Observer[Quality | Power] =
+    changeDieChanges(key, CharacterComputation.DieChange.Upgrade)
+
+  def downgrade(key: StagingKey): Observer[Quality | Power] =
+    changeDieChanges(key, CharacterComputation.DieChange.Downgrade)
+
   def allDieChanges(
       dcs: Map[StagingKey, Map[Quality | Power, DieChange]],
       mps: Option[PowerSource],
       mat: Option[Archetype],
       mpt: Option[Personality]
-  ): Map[Quality | Power, DieChange] = allDieChangesFunc(dcs, mps, mat, mpt)
+  ): Map[Quality | Power, DieChange] =
+    (mps.fold(List())(ps => dcs.get(ps).fold(List())(dcps => dcps.toList)) ++
+      mat.fold(List())(at => dcs.get(at).fold(List())(dcat => dcat.toList)) ++
+      mpt.fold(List())(pt =>
+        dcs.get(pt).fold(List())(dcpt => dcpt.toList)
+      )).toMap
 
-  // Interface implementation via constructor parameters - these become the interface properties
+  // Interface implementation - exposing internal computed values
   val basePersonalityQualities: Map[StagingKey, List[(Quality, Die)]] = basePersonalityQualitiesVal
   val powerSourceAbilities: List[(PowerSource, Map[AbilityKey, ChosenAbility])] = powerSourceAbilitiesVal
   val archetypeAbilities: List[(Archetype, Map[AbilityKey, ChosenAbility])] = archetypeAbilitiesVal
